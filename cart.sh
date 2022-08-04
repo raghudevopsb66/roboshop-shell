b7decf65383aee3040f7db9592e89c7124313ee0
Raghu K <raghuk.vit@gmail.com>
2022-08-04 08:39:59 +0530

    Update roboshop-shell/

--- cart.sh
+++ cart.sh
@@ -1,20 +1,21 @@
 set -e
 
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash >/tmp/cart.log
+yum install nodejs -y >/tmp/cart.log
 
-useradd roboshop
+useradd roboshop >/tmp/cart.log
 
-curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
-cd /home/roboshop
-rm -rf cart
-unzip -o /tmp/cart.zip
-mv cart-main cart
-cd cart
-npm install
+curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" >/tmp/cart.log
+cd /home/roboshop >/tmp/cart.log
+rm -rf cart >/tmp/cart.log
+unzip -o /tmp/cart.zip >/tmp/cart.log
+mv cart-main cart >/tmp/cart.log
+cd cart >/tmp/cart.log
+npm install >/tmp/cart.log
 
-mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
-systemctl daemon-reload
-systemctl start cart
-systemctl enable cart
+mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service >/tmp/cart.log
+systemctl daemon-reload >/tmp/cart.log
+systemctl start cart >/tmp/cart.log
+systemctl enable cart >/tmp/cart.log
+
 
