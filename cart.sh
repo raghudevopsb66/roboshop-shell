ac43b11809b585287164c1b96cf43ef572ec815e
Raghu K <raghuk.vit@gmail.com>
2022-08-06 08:27:59 +0530

    Update roboshop-shell/

--- cart.sh
+++ cart.sh
@@ -49,9 +49,7 @@ else
 fi
 
 echo Extract Application Archive
-unzip -o /tmp/cart.zip &>>/tmp/cart.log
-mv cart-main cart &>>/tmp/cart.log
-cd cart &>>/tmp/cart.log
+unzip -o /tmp/cart.zip &>>/tmp/cart.log && mv cart-main cart &>>/tmp/cart.log && cd cart &>>/tmp/cart.log
 if [ $? -eq 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
 else
@@ -69,8 +67,7 @@ else
 fi
 
 echo Configuring Cart SystemD Service
-mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>>/tmp/cart.log
-systemctl daemon-reload &>>/tmp/cart.log
+mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>>/tmp/cart.log && systemctl daemon-reload &>>/tmp/cart.log
 if [ $? -eq 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
 else
@@ -79,8 +76,7 @@ else
 fi
 
 echo Starting Cart Service
-systemctl start cart &>>/tmp/cart.log
-systemctl enable cart &>>/tmp/cart.log
+systemctl start cart &>>/tmp/cart.log && systemctl enable cart &>>/tmp/cart.log
 if [ $? -eq 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
 else
