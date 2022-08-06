01922082d0c66f4e8ec650cc240a33094f40884d
Raghu K <raghuk.vit@gmail.com>
2022-08-06 08:37:48 +0530

    Update roboshop-shell/

--- catalogue.sh
+++ catalogue.sh
@@ -1,11 +1,4 @@
-StatusCheck() {
-  if [ $? -eq 0 ]; then
-    echo -e "\e[32mSUCCESS\e[0m"
-  else
-    echo -e "\e[31mFAILURE\e[0m"
-    exit 1
-  fi
-}
+source common.sh
 
 echo "Condifuring NodeJS YUM Repos"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
