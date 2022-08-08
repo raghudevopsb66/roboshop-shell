50844a3b9f90596b32c62832d761b19236f7a9ce
Raghu K <raghuk.vit@gmail.com>
2022-08-08 07:55:40 +0530

    Update roboshop-shell/

--- redis.sh
+++ redis.sh
@@ -1,9 +1,18 @@
-set -e
+source common.sh
 
-curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
-yum install redis-6.2.7 -y
+COMPONENT=redis
+
+echo Setup YUM Repo
+curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>>${LOG}
+StatusCheck
+
+echo Install Redis
+yum install redis-6.2.7 -y &>>${LOG}
+StatusCheck
 
 # Update Listen IP
 
-systemctl enable redis
-systemctl start redis
+echo Start Redis Service
+systemctl enable redis &>>${LOG} && systemctl restart redis &>>${LOG}
+StatusCheck
+
