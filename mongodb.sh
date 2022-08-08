20caf0fc6e9bfad419ecd3281079f0039026ca6c
Raghu K <raghuk.vit@gmail.com>
2022-08-08 07:53:16 +0530

    Update roboshop-shell/

--- mongodb.sh
+++ mongodb.sh
@@ -1,15 +1,27 @@
-set -e
+source common.sh
 
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
-yum install -y mongodb-org
-systemctl enable mongod
-systemctl start mongod
-systemctl restart mongod
-curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+COMPONENT=mongodb
 
-cd /tmp
-unzip -o mongodb.zip
-cd mongodb-main
-mongo < catalogue.js
-mongo < users.js
+echo Setup YUM repo
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>${LOG}
+StatusCheck
 
+echo Install MongoDB
+yum install -y mongodb-org  &>>${LOG}
+StatusCheck
+
+echo Start MongoDB Service
+systemctl enable mongod &>>${LOG} && systemctl start mongod &>>${LOG}
+StatusCheck
+
+## Update the Listen config
+
+DOWNLOAD
+
+echo "Extract Schema Files"
+cd /tmp && unzip -o mongodb.zip &>>${LOG}
+StatusCheck
+
+echo Load Schema
+cd mongodb-main && mongo < catalogue.js &>>${LOG} && mongo < users.js &>>${LOG}
+StatusCheck
