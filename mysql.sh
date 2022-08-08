9dc91ce6e14c0ae7e80f98a80fe7368a5db58bd5
Raghu K <raghuk.vit@gmail.com>
2022-08-08 08:09:36 +0530

    Update roboshop-shell/

--- mysql.sh
+++ mysql.sh
@@ -19,17 +19,22 @@ systemctl enable mysqld &>>${LOG} && systemctl start mysqld &>>${LOG}
 StatusCheck
 
 echo "show databases;" | mysql -uroot -p$MYSQL_PASSWORD &>>${LOG}
-if [ $? -ne 0 ]; then 
+if [ $? -ne 0 ]; then
   echo Changing Default Password
   DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
   echo "alter user 'root'@'localhost' identified with mysql_native_password by '$MYSQL_PASSWORD';" | mysql --connect-expired-password -uroot -p${DEFAULT_PASSWORD}
   StatusCheck
 fi
 
-exit
-echo "uninstall plugin validate_password;" | mysql -uroot -p$MYSQL_PASSWORD
-#> uninstall plugin validate_password;
+echo "show plugins;" | mysql -uroot -p$MYSQL_PASSWORD | grep validate_password &>>${LOG}
+if [ $? -ne 0 ]; then
+  echo Remove Password Validate Plugin 
+  echo "uninstall plugin validate_password;" | mysql -uroot -p$MYSQL_PASSWORD
+  StatusCheck
+fi
 
+
+exit
 curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
 
 cd /tmp
