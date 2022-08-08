184a9308cf27960a5d14ebf6ebf5c7c25fd9f79f
Raghu K <raghuk.vit@gmail.com>
2022-08-08 07:58:40 +0530

    Update roboshop-shell/

--- mysql.sh
+++ mysql.sh
@@ -1,17 +1,23 @@
-set -e
+source common.sh
+COMPONENT=mysql
 
-curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
+echo Setup YUM Repo
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>${LOG}
+StatusCheck
 
-yum install mysql-community-server -y
+echo Install MySQL
+yum install mysql-community-server -y &>>${LOG}
+StatusCheck
 
-systemctl enable mysqld
-systemctl start mysqld
+echo Start MySQL Service
+systemctl enable mysqld &>>${LOG} && systemctl start mysqld &>>${LOG}
+StatusCheck
 
 DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
 
-echo MYSQL PASSWORD = $MYSQL_PASSWORD
 echo "alter user 'root'@'localhost' identified with mysql_native_password by '$MYSQL_PASSWORD';" | mysql --connect-expired-password -uroot -p${DEFAULT_PASSWORD}
 
+exit 
 echo "uninstall plugin validate_password;" | mysql -uroot -p$MYSQL_PASSWORD
 #> uninstall plugin validate_password;
 
