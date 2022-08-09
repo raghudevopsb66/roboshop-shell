2519055c18c41a8e7354fe9c2c7bb52355495244
Raghu K <raghuk.vit@gmail.com>
2022-08-09 08:46:54 +0530

    Update roboshop-shell/

--- frontend.sh
+++ frontend.sh
@@ -21,6 +21,8 @@ StatusCheck
 
 echo Updating Nginx Configuration
 sed -i -e '/catalogue/ s/localhost/catalogue-dev.roboshop.internal/' -e '/cart/ s/localhost/cart-dev.roboshop.internal/' -e '/user/ s/localhost/user-dev.roboshop.internal/' -e '/shipping/ s/localhost/shipping-dev.roboshop.internal/' -e '/payment/ s/localhost/payment-dev.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
+StatusCheck
 
 echo Start Nginx Service
 systemctl restart nginx &>>${LOG} && systemctl enable nginx &>>${LOG}
+StatusCheck
